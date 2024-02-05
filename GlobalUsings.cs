f3b674c5c7ab2199fac939dfb6c567a24e0b0b80
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2024-02-05 13:25:19 +0530

    Add project files.

---
+++ GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.CSharp;
+global using Microsoft.CodeAnalysis.CSharp.Syntax;
